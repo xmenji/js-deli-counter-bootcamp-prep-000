fd4eb204a714b25e95ecfe7b911891623359f4ef
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-18 01:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-  return "Number " + katzDeliLine.length;
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
 }
