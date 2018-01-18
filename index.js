22cdfbfa4d30feea049fb8e2c92a8fdf778f1361
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-18 01:18:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(katzDeliLine){
-  katzDeliLine.length > 0 ? 
-    "Currently serving " + katzDeliLine[0] :
-    return "There is nobody waiting to be served!";
+  if(katzDeliLine.length > 0){
+    return "Currently serving " + katzDeliLine[0];
+  }
+  return "There is nobody waiting to be served!";
     
 }
