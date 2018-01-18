9b63f27a5cb0e9fba15a7a8b4b7bd4f0cd81f5ca
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-18 01:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ function nowServing(katzDeliLine){
     customer = katzDeliLine.shift();  
     return "Currently serving " + customer + ".";
   }
+  
   return "There is nobody waiting to be served!";
-    
+}
+
+function currentLine(katzDeliLine){
+  var line = 'The line is currently: ';
+  
+  if(katzDeliLine.length !== 0){
+    for(var x = 0; x < katzDeliLine.length; x++){
+      line += x+1 + "." + katzDeliLine[x] + ", ";
+    }
+    return line;
+  }
+  return "The line is currently empty."
 }
